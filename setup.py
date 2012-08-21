12db20caec9134dbd1dc55fe7d68c68c6da95316
Toby Burress <kurin@delete.org>
2012-08-21 14:53:35 -0400

    Give distutils version info

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from distutils.core import setup
 from p2p import __version__
 
 setup(name='py-p2p',
+      version=__version__,
       description="A light peer-to-peer framework.",
       author="Toby Burress",
       author_email="kurin@delete.org",
