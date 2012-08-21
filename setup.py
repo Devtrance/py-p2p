09d0769b25c74c8c2d5553aa25dfb57106ec99bf
Toby Burress <kurin@delete.org>
2012-08-21 13:26:03 -0400

    Need a setup.py for pip to install from github.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from p2p import __version__
+
+setup(name='py-p2p',
+      description="A light peer-to-peer framework.",
+      author="Toby Burress",
+      author_email="kurin@delete.org",
+      packages=['p2p'])
