22a27ed1b9a64d0d0ee7a35f2ccb916589e38656
Toby Burress <kurin@delete.org>
2012-10-04 03:06:26 -0400

    Why can't I just use twisted like a sane person?

---
+++ p2p/tcp.py
@@ -0,0 +1,66 @@
+import socket
+import errno
+import thread
+import struct
+
+from event import Event
+
+class TCP(object):
+    def __init__(self, port):
+        self.port = port
+        self.handlers = Event()
+        self.connected = Event()
+
+    def start(self):
+        self.srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.srv.bind(("", self.port))
+        except socket.error as e:
+            if e.errno == errno.EADDRINUSE:
+                # something is already there; just bind to anything for now
+                self.sock.bind(("", 0))
+            else:
+                raise e
+        thread.start_new_thread(self.accept, ())
+
+    def connect(self, addr):
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn.connect(addr)
+        thread.start_new_thread(self.handle_conn, (conn, addr))
+
+    def accept(self):
+        self.srv.listen(5)
+        while True:
+            conn, addr = self.srv.accept()
+            thread.start_new_thread(self.handle_conn, (conn, addr))
+
+    def handle_conn(self, conn, addr):
+        self.connected.fire(conn, addr)
+        buff = ""
+        while True:
+            msg, buff = self.read_msg(conn, buff)
+            self.handlers.fire(msg, conn)
+
+    def read_msg(self, conn, buff=""):
+        while len(buff) < 4:
+            buff += conn.recv(1024)
+        msgsize = struct.unpack("!I", buff[0:4])[0]
+        buff = buff[4:]
+        while len(buff) < msgsize:
+            buff += conn.recv(1024)
+        msg = buff[:msgsize]
+        return msg, buff
+
+    def send(self, msg, conn):
+	    # pepper me with exceptions, for when TCP falls on its
+    	# stupid face
+        msgsize = struct.pack("!I", len(msg))
+        conn.send(msgsize)
+        conn.send(msg)
+
+    def shutdown(self):
+        try:
+            self.sock.shutdown(socket.SHUT_RDWR)
+            self.sock.close()
+        except: # nobody cares
+            pass
