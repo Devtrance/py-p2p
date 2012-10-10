1bc1dea002fb7d378ea022afcc0f73c4d1e57a08
Toby Burress <kurin@delete.org>
2012-10-09 22:21:10 -0400

    Re-redid Maekawa's algorithm.  Much more resiliant.

--- p2p/maekawa.py
+++ p2p/maekawa.py
@@ -1,35 +1,61 @@
 import uuid
 import heapq
+import threading
 
 class MaekawaNode(object):
     def __init__(self, parent):
+        self.lock = threading.RLock() # probably an unneeded mistake
         self.parent = parent  # this is so goddamn backwards
         self.acqcb = None     # the callback invoked when the mutex is acquired
         self.grant = None     # the id of the peer we have given our "grant" toekn
-        self.maeq = []        # a queue of requests
+        self.maeq = []        # a queue of requests, in the form of (sequence number, node id)
         self.mutexed = False  # whether we have the mutex
         self.fails = set()    # a set of peers who have rejected our request
-        self.yields = set()   # a set of peers we have yielded our request to
         self.grants = set()   # the peers we have grant tokens from
         self.grantseq = None  # the sequence number for the peer we have given our grant token
-        self.reqseq = None    # the sequence number for our grant request
+        self.reqseq = 0       # the sequence number for our grant request
         self.inquires = set() # a set of peers who have sent us 'inquire' messages, in case we decide to yield
+        self.inquired = False # whether we have an outstanding 'inquire' message with our current grant
 
     def acquire(self, acqcb=None):
-        self.acqcb = acqcb
-        msg = self.parent.mkmsg('maekawa')
-        msg['maekawa'] = 'request'
-        msg['seq'] = self.reqseq = uuid.uuid4().int
-        self.parent.broadcast(msg)
+        with self.lock:
+            self.acqcb = acqcb
+            self.inquires = set()
+            self.fails = set()
+            self.grants = set()
+            self.reqseq += 1
+            msg = self.parent.mkmsg('maekawa')
+            msg['maekawa'] = 'request'
+            msg['seq'] = self.reqseq
+            self.parent.broadcast(msg)
 
     def release(self):
-        if not self.mutexed:
-            return
-        self.mutexed = False
-        msg = self.parent.mkmsg('maekawa')
-        msg['maekawa'] = 'release'
-        msg['seq'] = self.reqseq
-        self.parent.broadcast(msg)
+        with self.lock:
+            if not self.mutexed:
+                return
+            self.mutexed = False
+            msg = self.parent.mkmsg('maekawa')
+            msg['maekawa'] = 'release'
+            msg['seq'] = self.reqseq
+            self.parent.broadcast(msg)
+
+    @staticmethod
+    def should_yield(msgid, msgseq, newid, newseq):
+        '''
+        The question, specifically, is "should msgid, msgseq yield to newid, newseq?"
+
+	    The rule is, lower sequence IDs get precidence, and when
+    	seqids are tied, lower msgids win.
+        '''
+        if msgseq < newseq:
+            return False
+        if newseq < msgseq:
+            return True
+        if msgid < newid:
+            return False
+        if newid < msgid:
+            return True
+        return False
 
     def handle_msg(self, msg):
         if not msg['maekawa']:
@@ -44,98 +70,111 @@ class MaekawaNode(object):
         f = " ".join(['recv', "%03d"%(msgid%997), ">>", "%03d"%(self.parent.uuid%997), msg['maekawa']])
         print f
         nmsg = self.parent.mkmsg('maekawa')
-        ans = handler(msg, msgid, nmsg)
+        with self.lock:
+            ans = handler(msg, msgid, nmsg)
         if ans:
             f = " ".join(['send', "%03d"%(self.parent.uuid%997), ">>", "%03d"%(ans[1]%997), ans[0]['maekawa']])
             print f
             self.parent.sendmsg(ans[0], ans[1])
 
     def handle_msg_request(self, msg, msgid, nmsg):
-        if not self.grant:
-            nmsg['maekawa'] = 'grant'
+        '''
+        Received a 'request' message.
+
+	    If self.grant is None (we have not granted anyone yet), set
+    	self.grant and send 'grant' message.
+
+	    If we have an outstanding grant from an ID with msgid that
+    	is lower, numerically, than the requesting msgid, then queue
+	    the request and send a 'fail' message back to the requesting
+    	msgid.
+
+	    If we have an outstanding grant and the requesting msgid
+    	is lower than the grant id, send an 'inquire' message to
+    	the node that has our grant.
+        '''
+        if self.grant is None:
+            # we have nothing outstanding -- grant the request
             self.grant = msgid
             self.grantseq = msg['seq']
-        else:
-            heapq.heappush(self.maeq, (msgid, msg['seq']))
-            if self.grant <= msgid:
-                # current grant has greater "priority", lower is better
-                nmsg['maekawa'] = 'fail'
-                nmsg['seq'] = msg['seq']
-            else:
-                msgid = self.grant
-                nmsg['maekawa'] = 'inquire'
-                nmsg['seq'] = self.grantseq
-        return nmsg, msgid
+            nmsg['seq'] = msg['seq']
+            nmsg['maekawa'] = 'grant'
+            self.inquired = False
+            return nmsg, msgid
+        # enqueue the message, since we can't grant it now
+        heapq.heappush(self.maeq, (msg['seq'], msgid))
+        if not self.should_yield(self.grant, self.grantseq, msgid, msg['seq']):
+            # the current grant has a higher priority; fail the request
+            nmsg['seq'] = msg['seq']
+            nmsg['maekawa'] = 'fail'
+            return nmsg, msgid
+        if not self.inquired:
+            # send an inquire, since we haven't yet
+            nmsg['maekawa'] = 'inquire'
+            nmsg['seq'] = self.grantseq
+            self.inquired = True
+            return nmsg, self.grant
+        # nothing to do, fall off the end
 
     def handle_msg_grant(self, msg, msgid, nmsg):
+        if msg['seq'] != self.reqseq:
+            return
         self.grants.add(msgid)
-        if msgid in self.fails:
-            self.fails.remove(msgid)
-        if msgid in self.yields:
-            self.yields.remove(msgid)
-        if len(self.grants) == len(self.parent.peers) + 1:
+        if len(self.grants) == len(self.parent.peers) + 1: #XXX find a better way to express this
             self.mutexed = True
-            self.fails = set()
-            self.yields = set()
-            self.grants = set()
-            self.inquires = set()
-            if self.acqcb:
-                self.acqcb()
-            self.acqcb = None
+            self.acqcb()
 
     def handle_msg_inquire(self, msg, msgid, nmsg):
-        if msg['seq'] != self.reqseq:
+        '''
+	    The 'inquire' message.  If we have any failures, we reply
+    	with a 'yield'.  Otherwise just store the request in case
+    	we get a failure at some other point.
+        '''
+        if not msg['seq'] == self.reqseq:
             return
-        if len(self.fails) > 0 or len(self.yields) > 0:
+        if len(self.fails) > 0:
             nmsg['maekawa'] = 'yield'
             nmsg['seq'] = self.reqseq
-            self.yields.add(msgid)
-            self.grants.remove(msgid)
+            return nmsg, msgid
         else:
-            self.inquires.add((msgid, self.grantseq))
-            return # no answer?
-        return nmsg, msgid
-
-    def handle_msg_yield(self, msg, msgid, nmsg):
-        if msg['seq'] != self.grantseq:
-            return
-        newmsgid, newseq = heapq.heappop(self.maeq)
-        heapq.heappush(self.maeq, (msgid, msg['seq']))
-        msgid = newmsgid
-        nmsg['maekawa'] = 'grant'
-        nmsg['seq'] = newseq
-        self.grant = msgid
-        self.grantseq = newseq
-        return nmsg, msgid
-
-    def handle_msg_release(self, msg, msgid, nmsg):
-        if msgid != self.grant or msg['seq'] != self.grantseq:
-            print "%03d I don't believe you"%(self.parent.uuid%997)
-            return
-        self.grant = None
-        self.grantseq = None
-        if len(self.maeq) == 0:
-            print "%03d noq"%(self.parent.uuid%997)
-            return
-        msgid, newseq = heapq.heappop(self.maeq)
-        self.grant = msgid
-        self.grantseq = newseq
-        nmsg['maekawa'] = 'grant'
-        nmsg['seq'] = newseq
-        return nmsg, msgid
+            self.inquires.add(msgid)
 
     def handle_msg_fail(self, msg, msgid, nmsg):
-        if msg['seq'] != self.reqseq:
+        if not msg['seq'] == self.reqseq:
             return
+        # send out yields for any cached inquires
+        for i in self.inquires:
+            t = self.parent.mkmsg('maekawa')
+            t['maekawa'] = 'yield'
+            t['seq'] = self.reqseq
+            self.parent.sendmsg(t, i)
+        self.inquires = set() # empty the set
         self.fails.add(msgid)
-        for i, s in self.inquires:
-            tmsg = self.parent.mkmsg('maekawa')
-            tmsg['maekawa'] = 'yield'
-            tmsg['seq'] = s
-            f = " ".join(['send', "%03d"%(self.parent.uuid%997), ">>", "%03d"%(i%997), tmsg['maekawa']])
-            print f
-            self.parent.sendmsg(tmsg, i)
-            self.yields.add(i)
-            if i in self.grants:
-                self.grants.remove(i)
-        self.inquires = set()
+
+    def handle_msg_yield(self, msg, msgid, nmsg):
+	    # the node holding our grant has yielded it, so give it to
+	    # whoever's next on the queue
+        if msg['seq'] != self.grantseq or msgid != self.grant:
+            return
+        heapq.heappush(self.maeq, (self.grantseq, self.grant))
+        seq, mid = heapq.heappop(self.maeq) # we won't get the same request back, 'cause heap
+        self.grant = mid
+        self.grantseq = seq
+        nmsg['seq'] = seq
+        nmsg['maekawa'] = 'grant'
+        self.inquired = False
+        return nmsg, mid
+
+    def handle_msg_release(self, msg, msgid, nmsg):
+        # unlock and send a grant to the next guy, if any
+        self.grant = None
+        self.grantseq = None
+        if len(self.maeq) > 0:
+            seq, mid = heapq.heappop(self.maeq)
+            self.grant = mid
+            self.grantseq = seq
+            nmsg['seq'] = seq
+            nmsg['maekawa'] = 'grant'
+            self.inquired = False
+            return nmsg, mid
+
